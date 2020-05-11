d5492df547560093ef53400888be3f8ea0d6e408
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-05-11 07:12:05 +0700

    tambah form register

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 <body>
 
 <h2>Login dengan modal</h2>
+<button onclick="document.getElementById('02').style.display='block'" style="width:auto;">Register</button>
+<button onclick="document.getElementById('01').style.display='block'" style="width:auto;">Login</button>
 
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-<div id="id01" class="modal">
+<div id="01" class="modal">
   
   <form class="modal-content animate" action="/action_page.php" method="post">
     <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <span onclick="document.getElementById('01').style.display='none'" class="close" title="Close Modal">&times;</span>
       <img src="avatar.png" alt="Avatar" class="avatar">
     </div>
 
@@ -32,15 +32,43 @@
     </div>
 
     <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <button type="button" onclick="document.getElementById('01').style.display='none'" class="cancelbtn">Cancel</button>
       <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+<div id="02" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('02').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label for="nama"><b>Name</b></label>
+      <input type="text" placeholder="Enter Your Name" name="nama" required>
+
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>
+
+      <label for="notelp"><b>Telephone</b></label>
+      <input type="text" placeholder="Enter Your Phone" name="notelp" required>
+        
+      <button type="submit">Register</button>
     </div>
   </form>
 </div>
 
 <script>
 // Inisialisasi modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById('01');
 
 // Otomatis menutup saat user click diluar box
 window.onclick = function(event) {
